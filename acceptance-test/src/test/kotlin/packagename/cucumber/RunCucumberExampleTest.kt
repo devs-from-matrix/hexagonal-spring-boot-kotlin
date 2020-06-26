681740362a46b610a3531b89f37a0d826541cd8a
Paul Williams <paul58914080@gmail.com>
2020-06-26 21:33:41 +0530

    fix: added CucumberContextConfiguration and updated tags
    Signed-off-by: Paul Williams <paul58914080@gmail.com>

--- acceptance-test/src/test/kotlin/packagename/cucumber/RunCucumberExampleTest.kt
+++ acceptance-test/src/test/kotlin/packagename/cucumber/RunCucumberExampleTest.kt
@@ -3,11 +3,14 @@ package packagename.cucumber
 import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootContextLoader
+import org.springframework.test.context.ContextConfiguration
+import packagename.ExampleE2EApplication
 
 @RunWith(Cucumber::class)
 @CucumberOptions(features = ["classpath:features/example.feature"],
     strict = true,
     plugin = ["json:target/cucumber/example.json", "json:target/cucumber/example.xml"],
-    tags = ["@Example"],
+    tags = "@Example",
     glue = ["classpath:packagename.cucumber"])
 class RunCucumberExampleTest
