11de4fe8747684eefc11c9c7963a2a34fbeed457
Paul Williams <paul58914080@gmail.com>
2021-07-19 21:56:37 +0530

    refactor: different schema for liquibase, transaction n audit tables

---
+++ jpa-adapter/src/main/kotlin/packagename/repository/entity/EnversRevisionEntity.kt
@@ -0,0 +1,18 @@
+package packagename.repository.entity
+
+import javax.persistence.*
+
+@Table(name = "REVISION_INFO", schema = "EXAMPLE_AUDIT")
+@Entity
+@SequenceGenerator(
+    schema = "EXAMPLE_AUDIT", name = "SEQ_REVISION_INFO",
+    sequenceName = "EXAMPLE_AUDIT.SEQ_REVISION_INFO", allocationSize = 1
+)
+data class EnversRevisionEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_REVISION_INFO")
+    @Column(name = "REV")
+    private val rev: Long? = null,
+    @Column(name = "TIMESTAMP")
+    private val code: Long? = null
+)
--- jpa-adapter/src/test/kotlin/packagename/repository/ExampleJpaAdapterApplication.kt
+++ jpa-adapter/src/test/kotlin/packagename/repository/ExampleJpaAdapterApplication.kt
@@ -3,25 +3,17 @@ package packagename.repository
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
-import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import packagename.domain.port.ObtainExample
-import packagename.repository.dao.ExampleDao
+import org.springframework.context.annotation.Import
+import packagename.repository.config.JpaAdapterConfig
 
 @SpringBootApplication
 class ExampleJpaAdapterApplication {
 
-  fun main(args: Array<String>) {
-    SpringApplication.run(ExampleJpaAdapterApplication::class.java, *args)
-  }
-
-  @TestConfiguration
-  @EnableJpaRepositories("packagename.repository.dao", repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean::class)
-  class ExampleJpaTestConfig {
-    @Bean
-    fun getObtainExampleRepository(exampleDao: ExampleDao): ObtainExample {
-      return ExampleRepository(exampleDao)
+    fun main(args: Array<String>) {
+        SpringApplication.run(ExampleJpaAdapterApplication::class.java)
     }
-  }
+
+    @TestConfiguration
+    @Import(JpaAdapterConfig::class)
+    class ExampleJpaTestConfig
 }
