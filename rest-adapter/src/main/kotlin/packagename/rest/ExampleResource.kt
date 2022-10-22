b4509b9db036e6ae8d4fdbc2b1ba6664882c1457
Paul Williams <paul58914080@gmail.com>
2022-10-22 12:25:40 +0530

    feat(rest-adapter): migrate use webflux

--- rest-adapter/src/main/kotlin/packagename/rest/ExampleResource.kt
+++ rest-adapter/src/main/kotlin/packagename/rest/ExampleResource.kt
@@ -8,18 +8,19 @@ import org.springframework.web.bind.annotation.RestController
 import packagename.domain.model.Example
 import packagename.domain.model.ExampleInfo
 import packagename.domain.port.RequestExample
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api/v1/examples")
 class ExampleResource(private val requestExample: RequestExample) {
 
   @GetMapping
-  fun getExamples(): ResponseEntity<ExampleInfo> {
-    return ResponseEntity.ok(requestExample.getExamples())
+  fun getExamples(): ResponseEntity<Mono<ExampleInfo>> {
+    return ResponseEntity.ok(Mono.just(requestExample.getExamples()))
   }
 
   @GetMapping(path = ["/{code}"])
-  fun getExampleByCode(@PathVariable code: Long): ResponseEntity<Example> {
-    return ResponseEntity.ok(requestExample.getExampleByCode(code))
+  fun getExampleByCode(@PathVariable code: Long): ResponseEntity<Mono<Example>> {
+    return ResponseEntity.ok(Mono.just(requestExample.getExampleByCode(code)))
   }
 }
--- rest-adapter/src/test/kotlin/packagename/rest/ExampleResourceTest.kt
+++ rest-adapter/src/test/kotlin/packagename/rest/ExampleResourceTest.kt
@@ -6,13 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.http.HttpStatus
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.test.web.reactive.server.WebTestClient
 import packagename.domain.exception.ExampleNotFoundException
 import packagename.domain.model.Example
 import packagename.domain.model.ExampleInfo
@@ -20,24 +15,20 @@ import packagename.domain.port.RequestExample
 import packagename.rest.exception.ExampleExceptionResponse
 
 @ExtendWith(MockitoExtension::class)
-@SpringBootTest(classes = [ExamplePoetryRestAdapterApplication::class], webEnvironment = RANDOM_PORT)
-@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
+@WebFluxTest(ExampleResource::class)
 class ExampleResourceTest {
   companion object {
-    private const val LOCALHOST = "http://localhost:"
     private const val API_URI = "/api/v1/examples"
   }
 
-  @LocalServerPort
-  private val port: Int = 0
   @Autowired
-  private lateinit var restTemplate: TestRestTemplate
+  private lateinit var webTestClient: WebTestClient
   @Autowired
   private lateinit var requestExample: RequestExample
 
   @Test
   fun `should start the rest adapter application`() {
-    assertThat(java.lang.Boolean.TRUE).isTrue()
+    assertThat(java.lang.Boolean.TRUE).isTrue
   }
 
   @Test
@@ -45,12 +36,14 @@ class ExampleResourceTest {
     // Given
     Mockito.lenient().`when`(requestExample.getExamples()).thenReturn(mockExampleInfo())
     // When
-    val url = "$LOCALHOST$port$API_URI"
-    val responseEntity = restTemplate.getForEntity(url, ExampleInfo::class.java)
-    // Then
-    assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
-    assertThat(responseEntity.body).isNotNull
-    assertThat(responseEntity.body.examples).isNotEmpty.extracting("description").contains("Johnny Johnny Yes Papa !!")
+    webTestClient
+            .get()
+            .uri(API_URI)
+            .exchange()
+            // Then
+            .expectStatus().isOk
+            .expectBody(ExampleInfo::class.java)
+            .consumeWith { assertThat(it.responseBody?.examples).isNotEmpty.extracting("description").contains("Johnny Johnny Yes Papa !!") }
   }
 
   @Test
@@ -60,12 +53,16 @@ class ExampleResourceTest {
     val description = "Twinkle Twinkle"
     Mockito.lenient().`when`(requestExample.getExampleByCode(code)).thenReturn(mockExample(code, description))
     // When
-    val url = "$LOCALHOST$port$API_URI/$code"
-    val responseEntity = restTemplate.getForEntity(url, Example::class.java)
-    // Then
-    assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
-    assertThat(responseEntity.body).isNotNull
-    assertThat(responseEntity.body).isNotNull.isEqualTo(mockExample(code, description))
+    webTestClient
+            .get()
+            .uri("$API_URI/$code")
+            .exchange()
+            // Then
+            .expectStatus().isOk
+            .expectBody(Example::class.java)
+            .consumeWith {
+              assertThat(it.responseBody).isNotNull.isEqualTo(mockExample(code, description))
+            }
   }
 
   @Test
@@ -74,12 +71,16 @@ class ExampleResourceTest {
     val code = -1000L
     Mockito.lenient().`when`(requestExample.getExampleByCode(code)).thenThrow(ExampleNotFoundException(code))
     // When
-    val url = "$LOCALHOST$port$API_URI/$code"
-    val responseEntity = restTemplate.getForEntity(url, ExampleExceptionResponse::class.java)
-    // Then
-    assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
-    assertThat(responseEntity.body).isNotNull
-    assertThat(responseEntity.body).isEqualTo(ExampleExceptionResponse("Example with code: [$code] does not exists", "$API_URI/$code"))
+    webTestClient
+            .get()
+            .uri("$API_URI/$code")
+            .exchange()
+            // Then
+            .expectStatus().isNotFound
+            .expectBody(ExampleExceptionResponse::class.java)
+            .consumeWith {
+              assertThat(it.responseBody).isNotNull.isEqualTo(ExampleExceptionResponse("Example with code: [$code] does not exists", "$API_URI/$code"))
+            }
   }
 
   private fun mockExample(code: Long, description: String): Example {
