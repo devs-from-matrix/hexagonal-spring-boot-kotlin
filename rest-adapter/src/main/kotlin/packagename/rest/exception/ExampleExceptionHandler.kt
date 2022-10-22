6fb47fe30776b929fc28ee72ced523b646211033
Paul Williams <paul58914080@gmail.com>
2022-10-22 12:25:40 +0530

    feat(rest-adapter): migrate use webflux

--- rest-adapter/src/main/kotlin/packagename/rest/exception/ExampleExceptionHandler.kt
+++ rest-adapter/src/main/kotlin/packagename/rest/exception/ExampleExceptionHandler.kt
@@ -2,18 +2,17 @@ package packagename.rest.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.context.request.ServletWebRequest
-import org.springframework.web.context.request.WebRequest
 import packagename.domain.exception.ExampleNotFoundException
-import java.lang.Exception
+import reactor.core.publisher.Mono
 
 @RestControllerAdvice(basePackages = ["packagename"])
 class ExampleExceptionHandler {
 
-  @ExceptionHandler(value = [ExampleNotFoundException::class])
-  fun handleExampleNotFoundException(exception: Exception, request: WebRequest): ResponseEntity<ExampleExceptionResponse> {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExampleExceptionResponse(exception.message, (request as ServletWebRequest).request.requestURI))
-  }
+    @ExceptionHandler(value = [ExampleNotFoundException::class])
+    fun handleExampleNotFoundException(exception: Exception, request: ServerHttpRequest): ResponseEntity<Mono<ExampleExceptionResponse>> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Mono.just(ExampleExceptionResponse(exception.message, request.uri.path)))
+    }
 }
