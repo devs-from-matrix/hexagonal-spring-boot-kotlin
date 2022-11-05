03520f6866d4c1d4fd29aa80496a47b3b4ef4e68
Paul Williams <paul58914080@gmail.com>
2022-11-05 12:36:57 +0530

    build(r2dbc-adapter): add simple test to persist data

---
+++ r2dbc-adapter/src/test/kotlin/packagename/respository/ExampleR2dbcAdapterTestApplication.kt
@@ -0,0 +1,11 @@
+package packagename.respository
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ExampleR2dbcAdapterTestApplication {
+  fun main(args: Array<String>) {
+    SpringApplication.run(ExampleR2dbcAdapterTestApplication::class.java)
+  }
+}
