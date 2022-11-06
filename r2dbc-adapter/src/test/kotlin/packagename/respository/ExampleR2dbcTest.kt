cfe3387671daa6557185f6d35d5b2b39642214a7
Paul Williams <paul58914080@gmail.com>
2022-11-06 16:06:28 +0530

    feat(r2dbc-adapter): integration with liquibase

--- r2dbc-adapter/src/test/kotlin/packagename/respository/ExampleR2dbcTest.kt
+++ r2dbc-adapter/src/test/kotlin/packagename/respository/ExampleR2dbcTest.kt
@@ -1,14 +1,19 @@
 package packagename.respository
 
+import net.lbruun.springboot.preliquibase.PreLiquibaseAutoConfiguration
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.test.context.ActiveProfiles
 import reactor.test.StepVerifier
 import java.util.function.Consumer
 
 
 @SpringBootTest
+@ImportAutoConfiguration(PreLiquibaseAutoConfiguration::class)
+@ActiveProfiles("test")
 class ExampleR2dbcTest {
 
   @Autowired
--- r2dbc-adapter/src/test/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO EXAMPLE.T_EXAMPLE(TECH_ID, CODE, DESCRIPTION) VALUES
-(1000, 1, 'Twinkle twinkle little star');
