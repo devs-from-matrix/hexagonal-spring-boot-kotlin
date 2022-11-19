c108aabb159c5c0e918247662a5f35043d58b7f4
Paul Williams <paul58914080@gmail.com>
2022-11-19 19:01:27 +0530

    refactor(r2dbc-adapter): remove preliquibase-spring-boot-starter

--- r2dbc-adapter/src/main/kotlin/packagename/repository/config/R2dbcAdapterConfig.kt
+++ r2dbc-adapter/src/main/kotlin/packagename/repository/config/R2dbcAdapterConfig.kt
@@ -1,9 +1,14 @@
 package packagename.repository.config
 
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 import packagename.domain.port.ObtainExample
 import packagename.repository.ExampleRepository
 import packagename.repository.dao.ExampleDao
@@ -17,5 +22,15 @@ class R2dbcAdapterConfig {
   @Bean
   fun getExampleRepository(exampleDao: ExampleDao): ObtainExample {
     return ExampleRepository(exampleDao)
+  }
+
+  @Bean
+  fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+    val initializer = ConnectionFactoryInitializer()
+    initializer.setConnectionFactory(connectionFactory)
+    val populator = CompositeDatabasePopulator()
+    populator.addPopulators(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+    initializer.setDatabasePopulator(populator)
+    return initializer
   }
 }
