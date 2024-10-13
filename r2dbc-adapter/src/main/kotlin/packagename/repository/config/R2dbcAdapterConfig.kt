5baccf1585f3a9d72949cd41405e0550624675e6
Paul Williams <paul58914080@gmail.com>
2024-10-13 11:44:15 +0530

    fix: load r2dbc config and schema.sql first before liquibase
    remove username and password

--- r2dbc-adapter/src/main/kotlin/packagename/repository/config/R2dbcAdapterConfig.kt
+++ r2dbc-adapter/src/main/kotlin/packagename/repository/config/R2dbcAdapterConfig.kt
@@ -1,9 +1,14 @@
 package packagename.repository.config
 
 import io.r2dbc.spi.ConnectionFactory
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
+import org.springframework.boot.r2dbc.ConnectionFactoryBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.core.annotation.Order
 import org.springframework.core.io.ClassPathResource
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
@@ -24,8 +29,16 @@ class R2dbcAdapterConfig {
     return ExampleRepository(exampleDao)
   }
 
+  @Bean("myConnectionFactory")
+  @Primary
+  fun connectionFactory(r2dbcProperties: R2dbcProperties): ConnectionFactory =
+    ConnectionFactoryBuilder
+      .withUrl("r2dbc:h2:mem:///h2database?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+      .build()
+
   @Bean
-  fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+  @Order(0) // to make sure this initializer runs first before liquibase
+  fun initializer(@Qualifier("myConnectionFactory") connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
     val initializer = ConnectionFactoryInitializer()
     initializer.setConnectionFactory(connectionFactory)
     val populator = CompositeDatabasePopulator()
